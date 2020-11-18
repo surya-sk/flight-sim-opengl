0ff611d1049b44c7164d8b25fca99650bbed0016
Surya <surya.sk05@outlook.com>
2020-11-18 15:31:43 +0530

    Setup camera movement

--- flight-sim-opengl/FlightSim.c
+++ flight-sim-opengl/FlightSim.c
@@ -12,6 +12,9 @@ Author:			Surya Kashyap
 //include library headers
 #include <freeglut.h>
 #include <stdio.h>
+#include <string.h>
+
+#pragma warning(disable:4996)
 
 // window dimensions
 GLint windowHeight = 600;
@@ -19,6 +22,12 @@ GLint windowWidth = 600;
 
 // camera position
 GLfloat cameraPosition[] = { 0.0, 0.0, 2.5 };
+
+// difference added at each frame
+GLfloat interpDiff = 0.003;
+
+// determine direction to move the camera in
+GLint moveUp, moveDown, moveRight, moveLeft, moveForward, moveBackward = 0;
 
 // size of the grid of quads
 const GLint gridSize = 100;
@@ -28,6 +37,26 @@ GLint goFullScreen = 0;
 
 // toggle polygon mode
 GLint showWireFrame = 0;
+
+// plane vertices, normals and faces
+GLfloat planeVertices[6763][3]; planeNormals[6763][3];
+GLint planeFaces[][3];
+
+// number of vertices, normals and faces
+GLint numCVertices, numCNormals, numCFaces;
+
+typedef struct SubObjects
+{
+	GLfloat vertices[3639][6];
+} SubObject;
+
+struct SubObject* subobjects[33];
+
+// file stream to access directory 
+FILE *fileStream;
+char fileText[100];
+
+int arr[3639][6];
 
 /************************************************************************
 
@@ -64,6 +93,63 @@ void initializeGL()
 
 	// enable depth testing
 	glEnable(GL_DEPTH_TEST);
+
+	// open the file
+	fileStream = fopen("cessna.txt", "r");
+
+	GLfloat x, y, z;
+	int xFace, yFace, zFace;
+	int i = 0, j = 0, k = 0, l = 0;
+
+	while (fgets(fileText, sizeof fileText, fileStream))
+	{
+		if (fileText[0] == 'v')
+		{
+			if (sscanf(fileText, "v %f %f %f", &x, &y, &z) != 3) { break; }
+			planeVertices[i][0] = x;
+			planeVertices[i][1] = y;
+			planeVertices[i][2] = z;
+			i++;
+		}
+		else if (fileText[0] == 'n')
+		{
+			if (sscanf(fileText, "n %f %f %f", &x, &y, &z) != 3) { break; }
+			planeNormals[j][0] = x;
+			planeNormals[j][1] = y;
+			planeNormals[j][2] = z;
+			j++;
+		}
+		else if (fileText[0] == 'g')
+		{
+			k++;
+		}
+		else if (fileText[0] == 'f')
+		{
+			//char delim[] = " ";
+			//char *ptr = strtok(fileText, delim);
+			//SubObject object;
+			//int value;
+			//int i = 0;
+			//while (ptr != NULL)
+			//{
+			//	//printf("'%s'\n", ptr);
+			//	if (ptr[0] != 'f')
+			//	{
+			//		sscanf(ptr, "%04d", &value);
+			//		printf("%d %d\n", l, i);
+			//		arr[l][i] = value;
+			//		object.vertices[l][i] = value;
+			//		i++;
+			//	}
+			//	ptr = strtok(NULL, delim);
+			//}
+		}
+	}
+
+
+	numCVertices = i;
+	numCNormals = j;
+	printf("%d", k);
 }
 
 /************************************************************************
@@ -83,9 +169,9 @@ void myDisplay()
 
 
 	// set to projection mode 
-	//glMatrixMode(GL_MODELVIEW);
+	glMatrixMode(GL_MODELVIEW);
 
-	//glLoadIdentity();
+	glLoadIdentity();
 
 	// set the camera position
 	gluLookAt(cameraPosition[0], cameraPosition[1], cameraPosition[2],
@@ -118,28 +204,139 @@ void myDisplay()
 	glTranslatef(0.0, 0.0, 0.0);
 	gluSphere(quad, 0.02, 25, 100);
 
-	//glTranslatef(-cameraPosition[0], -cameraPosition[1], -cameraPosition[2]);
 
 	glPushMatrix();
-	glTranslatef(0.0, - 0.5, 0.0);
+	glTranslatef(-0.5,  -0.5, -1.5);
 	glBegin(GL_QUADS);
-	for (float i = 0; i < gridSize; i += 0.1)
+	for (float i = 0; i < gridSize; i += 0.5)
 	{
 		float t = i;
-		for (float j = 0; j < gridSize; j += 0.1)
+		for (float j = 0; j < gridSize; j += 0.5)
 		{
 			float s = j;
 			glVertex3f(t, 0,s);
-			glVertex3f(t + 0.1, 0, s);
-			glVertex3f(t + 0.1, 0, s + 0.1);
-			glVertex3f(t,0, s + 0.1);
+			glVertex3f(t + 0.5, 0, s);
+			glVertex3f(t + 0.5, 0, s + 0.5);
+			glVertex3f(t,0, s + 0.5);
 		}
 	}
 	glEnd();
 	glPopMatrix();
 
+	glTranslatef(-cameraPosition[0], -cameraPosition[1], -cameraPosition[2]);
+
 	glutSwapBuffers();
 
+}
+
+/************************************************************************
+
+
+Function:		specialKeys
+
+
+Description:	Handles special key press functionality
+
+
+*************************************************************************/
+void specialKeys(int key, int x, int y)
+{
+	switch (key)
+	{
+	case GLUT_KEY_RIGHT:
+		moveRight = 1;
+		break;
+	case GLUT_KEY_LEFT:
+		moveLeft = 1;
+		break;
+	case GLUT_KEY_UP:
+		moveUp = 1;
+		break;
+	case GLUT_KEY_DOWN:
+		moveDown = 1;
+		break;
+	case GLUT_KEY_PAGE_UP:
+		moveForward = 1;
+		break;
+	case GLUT_KEY_PAGE_DOWN:
+		moveBackward = 1;
+		break;
+	}
+
+}
+
+/************************************************************************
+
+
+Function:		determineMovement
+
+
+Description:	 Determines movement based on key presses
+
+
+*************************************************************************/
+void determineMovement()
+{
+	// map camera movement to keys
+	if (moveRight)
+	{
+		cameraPosition[0] += interpDiff;
+	}
+	if (moveLeft)
+	{
+		cameraPosition[0] -= interpDiff;
+	}
+	if (moveUp)
+	{
+		cameraPosition[1] += interpDiff;
+	}
+	if (moveDown)
+	{
+		cameraPosition[1] -= interpDiff;
+	}
+	if (moveForward)
+	{
+		cameraPosition[2] -= interpDiff;
+	}
+	if (moveBackward)
+	{
+		cameraPosition[2] += interpDiff;
+	}
+}
+
+/************************************************************************
+
+
+Function:		myKeyUp
+
+
+Description:	Handles key release functionality
+
+
+*************************************************************************/
+void myKeyUp(int key, int x, int y)
+{
+	switch (key)
+	{
+	case GLUT_KEY_RIGHT:
+		moveRight = 0;
+		break;
+	case GLUT_KEY_LEFT:
+		moveLeft = 0;
+		break;
+	case GLUT_KEY_UP:
+		moveUp = 0;
+		break;
+	case GLUT_KEY_DOWN:
+		moveDown = 0;
+		break;
+	case GLUT_KEY_PAGE_UP:
+		moveForward = 0;
+		break;
+	case GLUT_KEY_PAGE_DOWN:
+		moveBackward = 0;
+		break;
+	}
 }
 
 /************************************************************************
@@ -160,6 +357,9 @@ void myIdle()
 	{
 		glutLeaveFullScreen();
 	}
+	determineMovement();
+
+	glutPostRedisplay();
 }
 
 /************************************************************************
@@ -255,6 +455,12 @@ void main(int argc, char** argv)
 
 	// set the keyboard function
 	glutKeyboardFunc(myKey);
+
+	// when key has been released
+	glutSpecialUpFunc(myKeyUp);
+
+	// special keys function
+	glutSpecialFunc(specialKeys);
 
 	// initialize the rendering context
 	initializeGL();
